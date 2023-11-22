4ae6e524ac1556f4bc0a465405903965c7af61d6
Fabio Buesser <fabio.bsser@hotmail.com>
2023-11-22 12:09:18 +0100

    review transactions

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TransactionRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,9 +20,12 @@ class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(
         UserRepository $userRepository,
+        TransactionRepository $transactionRepository,
     ): Response
     {
+        $reviewTransactions = $transactionRepository->findBy(['status' => 'review'], ['created' => 'DESC']);
         return $this->render('dashboard/index.html.twig', [
+            'reviewTransactions' => $reviewTransactions,
             'users' => $userRepository->findAll(),
         ]);
     }
